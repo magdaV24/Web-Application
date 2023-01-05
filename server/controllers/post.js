9479091f27102edbbcd95b7777b3353f8ef0b380
Magda <114444914+magdaV24@users.noreply.github.com>
2023-01-05 11:28:53 +0200

    Update post.js

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -24,25 +24,15 @@ export const getPost = (req, res) => {
 // }
 
 export const editPost = (req, res) => {
-  const token = req.cookies.access_token;
-  if (!token) return res.status(401).json("Not authenticated!");
 
-  jwt.verify(token, "jwtkey", (err, username) => {
-    if (err) return res.status(403).json("Token is not valid!");
+  const id = req.params.id;
+  const q = `UPDATE posts SET title = ?,  content = ? WHERE id = ${id}`;
 
-    const q = "UPDATE posts SET content = ?, title = ? WHERE id = ?";
+  const title = req.body.title;
+  const content = req.body.content;
 
-    const title = req.body.title;
-    const content = req.body.content;
-    //const id = req.body.id
-
-    const id = req.params.id;
-
-    const edit = [title, content];
-
-    db.query(q, [...edit, id, username], (err, result) => {
-      if (err) throw err;
-      res.send("The new values had been sent to the database!");
-    });
+  db.query(q, [title, content], (err, result) => {
+    if (err) throw err;
+    res.send("The new values had been sent to the database!");
   });
 };
